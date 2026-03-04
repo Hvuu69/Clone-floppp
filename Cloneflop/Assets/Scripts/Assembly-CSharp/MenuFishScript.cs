093745b8cdcafbd3209dff012cb61b5bf3b7a6a6
Hvuu36 <hmvu157@gmail.com>
2026-03-04 10:38:07 +0700

    h

--- Cloneflop/Assets/Scripts/Assembly-CSharp/MenuFishScript.cs
+++ Cloneflop/Assets/Scripts/Assembly-CSharp/MenuFishScript.cs
@@ -2,62 +2,56 @@ using UnityEngine;
 
 public class MenuFishScript : MonoBehaviour
 {
-	/*
-	Dummy class. This could have happened for several reasons:
+	public float baseSpeed = 1.5f;      // tốc độ chính
+	public float amplitude = 0.5f;      // biên độ lượn
+	public float frequency = 1f;        // tần số lượn
+	public float changeDirectionTime = 3f; // thời gian đổi hướng
 
-	1. No dll files were provided to AssetRipper.
+	private Vector3 direction;
+	private float timer = 0f;
+	private float offset;
 
-		Unity asset bundles and serialized files do not contain script information to decompile.
-			* For Mono games, that information is contained in .NET dll files.
-			* For Il2Cpp games, that information is contained in compiled C++ assemblies and the global metadata.
-			
-		AssetRipper usually expects games to conform to a normal file structure for Unity games of that platform.
-		A unexpected file structure could cause AssetRipper to not find the required files.
+	void Start()
+	{
+		// random phase để mỗi con cá có pattern khác nhau
+		offset = Random.Range(0f, 2f * Mathf.PI);
+		SetNewDirection();
+	}
 
-	2. Incorrect dll files were provided to AssetRipper.
+	void Update()
+	{
+		// timer để đổi hướng định kỳ
+		timer += Time.deltaTime;
 
-		Any of the following could cause this:
-			* Il2CppInterop assemblies
-			* Deobfuscated assemblies
-			* Older assemblies (compared to when the bundle was built)
-			* Newer assemblies (compared to when the bundle was built)
+		if (timer >= changeDirectionTime)
+		{
+			SetNewDirection();
+			timer = 0f;
+		}
 
-		Note: Although assembly publicizing is bad, it alone cannot cause empty scripts. See: https://github.com/AssetRipper/AssetRipper/issues/653
+		MoveFish();
+	}
 
-	3. Assembly Reconstruction has not been implemented.
+	void SetNewDirection()
+	{
+		// random góc 360 độ
+		float angle = Random.Range(0f, 2f * Mathf.PI);
+		direction = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0).normalized;
+	}
 
-		Asset bundles contain a small amount of information about the script content.
-		This information can be used to recover the serializable fields of a script.
+	void MoveFish()
+	{
+		float sine = Mathf.Sin(Time.time * frequency + offset) * amplitude;
 
-		See: https://github.com/AssetRipper/AssetRipper/issues/655
+		// di chuyển dựa vào direction và thêm lượn theo sine định hướng vuông góc
+		Vector3 perp = new Vector3(-direction.y, direction.x, 0);
 
-	4. This script is unnecessary.
+		transform.position += (direction * baseSpeed + perp * sine) * Time.deltaTime;
 
-		If this script has no asset or script references, it can be deleted.
-		Be sure to resolve any compile errors before deleting because they can hide references.
-
-	5. Script Content Level 0
-
-		AssetRipper was set to not load any script information.
-
-	6. Cpp2IL failed to decompile Il2Cpp data
-
-		If this happened, there will be errors in the AssetRipper.log indicating that it happened.
-		This is an upstream problem, and the AssetRipper developer has very little control over it.
-		Please post a GitHub issue at: https://github.com/SamboyCoding/Cpp2IL/issues
-
-	7. An incorrect path was provided to AssetRipper.
-
-		This is characterized by "Mixed game structure has been found at" in the AssetRipper.log file.
-		AssetRipper expects games to conform to a normal file structure for Unity games of that platform.
-		An unexpected file structure could cause AssetRipper to not find the required files for script decompilation.
-		Generally, AssetRipper expects users to provide the root folder of the game. For example:
-			* Windows: the folder containing the game's .exe file
-			* Mac: the .app file/folder
-			* Linux: the folder containing the game's executable file
-			* Android: the apk file
-			* iOS: the ipa file
-			* Switch: the folder containing exefs and romfs
-
-	*/
+		// lật sprite theo hướng chính
+		if (direction.x > 0)
+			transform.localScale = new Vector3(1, 1, 1);
+		else
+			transform.localScale = new Vector3(-1, 1, 1);
+	}
 }
--- Cloneflop/Assets/Scripts/Assembly-CSharp/PipeControllerScript.cs
+++ Cloneflop/Assets/Scripts/Assembly-CSharp/PipeControllerScript.cs
@@ -2,62 +2,48 @@ using UnityEngine;
 
 public class PipeControllerScript : MonoBehaviour
 {
-	/*
-	Dummy class. This could have happened for several reasons:
+	public GameObject pipePrefab;   // Prefab ống nước
+	public float spawnRate = 2f;    // Thời gian spawn (giây)
+	public float heightOffset = 2f; // Độ lệch cao thấp
 
-	1. No dll files were provided to AssetRipper.
+	public float safeMargin = 2.2f; // Khoảng cách an toàn trên dưới
 
-		Unity asset bundles and serialized files do not contain script information to decompile.
-			* For Mono games, that information is contained in .NET dll files.
-			* For Il2Cpp games, that information is contained in compiled C++ assemblies and the global metadata.
-			
-		AssetRipper usually expects games to conform to a normal file structure for Unity games of that platform.
-		A unexpected file structure could cause AssetRipper to not find the required files.
+	private float timer = 0f;
 
-	2. Incorrect dll files were provided to AssetRipper.
+	void Update()
+	{
+		if (timer < spawnRate)
+		{
+			timer += Time.deltaTime;
+		}
+		else
+		{
+			SpawnPipe();
+			timer = 0f;
+		}
+	}
 
-		Any of the following could cause this:
-			* Il2CppInterop assemblies
-			* Deobfuscated assemblies
-			* Older assemblies (compared to when the bundle was built)
-			* Newer assemblies (compared to when the bundle was built)
+	void SpawnPipe()
+	{
+		Camera cam = Camera.main;
 
-		Note: Although assembly publicizing is bad, it alone cannot cause empty scripts. See: https://github.com/AssetRipper/AssetRipper/issues/653
+		float screenBottom = cam.transform.position.y - cam.orthographicSize;
+		float screenTop = cam.transform.position.y + cam.orthographicSize;
 
-	3. Assembly Reconstruction has not been implemented.
+		float safeMargin = 2.2f;
 
-		Asset bundles contain a small amount of information about the script content.
-		This information can be used to recover the serializable fields of a script.
+		float minY = screenBottom + safeMargin;
+		float maxY = screenTop - safeMargin;
 
-		See: https://github.com/AssetRipper/AssetRipper/issues/655
+		float spawnY = Random.Range(minY, maxY);
 
-	4. This script is unnecessary.
+		float rightEdge = cam.transform.position.x
+						  + cam.orthographicSize * cam.aspect;
 
-		If this script has no asset or script references, it can be deleted.
-		Be sure to resolve any compile errors before deleting because they can hide references.
+		float spawnX = rightEdge + 2f;
 
-	5. Script Content Level 0
-
-		AssetRipper was set to not load any script information.
-
-	6. Cpp2IL failed to decompile Il2Cpp data
-
-		If this happened, there will be errors in the AssetRipper.log indicating that it happened.
-		This is an upstream problem, and the AssetRipper developer has very little control over it.
-		Please post a GitHub issue at: https://github.com/SamboyCoding/Cpp2IL/issues
-
-	7. An incorrect path was provided to AssetRipper.
-
-		This is characterized by "Mixed game structure has been found at" in the AssetRipper.log file.
-		AssetRipper expects games to conform to a normal file structure for Unity games of that platform.
-		An unexpected file structure could cause AssetRipper to not find the required files for script decompilation.
-		Generally, AssetRipper expects users to provide the root folder of the game. For example:
-			* Windows: the folder containing the game's .exe file
-			* Mac: the .app file/folder
-			* Linux: the folder containing the game's executable file
-			* Android: the apk file
-			* iOS: the ipa file
-			* Switch: the folder containing exefs and romfs
-
-	*/
+		Instantiate(pipePrefab,
+			new Vector3(spawnX, spawnY, 0),
+			Quaternion.identity);
+	}
 }
